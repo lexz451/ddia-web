df21b2640e9b7af9c6f3fa5a447ea05d33f98c97
Alejandro R. Zaldivar <lexz.zaldivar@gmail.com>
2023-09-29 17:30:21 -0700

    feat: Initial config and layout

---
+++ src/lib/components/get-involved.tsx
@@ -0,0 +1,7 @@
+export default function GetInvolved() {
+    return (
+        <div className="Rectangle157 bg-sky-900 rounded-3xl">
+            
+        </div>
+    )
+}
---
+++ src/lib/components/navbar.tsx
@@ -0,0 +1,36 @@
+import Logo from '@/lib/assets/logo.svg';
+import GlobeIcon from '@/lib/assets/globe-alt.svg';
+import SearchIcon from '@/lib/assets/search.svg';
+import Link from 'next/link';
+
+export default function Navbar() {
+    return (
+        <header className='absolute left-0 right-0'>
+            <nav className='flex items-center container mx-auto py-5'>
+                <div className='flex-1'>
+                    <Link href="/">
+                        <Logo className="w-32 h-20 relative"></Logo>
+                    </Link>
+                </div>
+                <ul className='inline-flex mr-10'>
+                    <li>
+                        <a className="Heading w-20 text-center text-sky-900 text-sm font-medium font-['Avenir'] uppercase leading-10">About us</a>
+                    </li>
+                    <li>
+                        <div className="Heading w-28 text-center text-sky-900 text-sm font-medium font-['Avenir'] uppercase leading-10">Our work</div>
+                    </li>
+                    <li>
+                        <div className="Heading w-36 text-center text-sky-900 text-sm font-medium font-['Avenir'] uppercase leading-10">meet the team</div>
+                    </li>
+                    <li>
+                        <div className="Heading w-32 text-center text-sky-900 text-sm font-medium font-['Avenir'] uppercase leading-10">Latest Updates</div>
+                    </li>
+                </ul>
+                <div className='flex items-center gap-4'>
+                    <SearchIcon className="w-6 h-6"></SearchIcon>
+                    <GlobeIcon className="w-6 h-6"></GlobeIcon>
+                </div>
+            </nav>
+        </header>
+    );
+};
